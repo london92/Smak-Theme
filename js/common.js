b7d6ae65aae428fb5fcf8573f236750dffc24031
Nikolai Chmyr <nikolai.chmyr@gmail.com>
2016-08-14 18:02:27 +0300

    add some fixes

--- js/common.js
+++ js/common.js
@@ -1,6 +1,3 @@
-/**
- * Created by Admin on 06.08.2016.
- */
 window.onload = function () {
     
     //для того, чтобы секция header автоматически вотображалась на всю высоту экрана 
@@ -278,91 +275,78 @@ window.onload = function () {
     }
 
     //Круговая диаграмма
-    //canvas initialization
-    var canvas = document.getElementsByTagName("canvas")[0];
-    var canvas_context = canvas.getContext("2d");
-    //dimensions
-    var W = canvas.width;
-    var H = canvas.height;
-    //Variables
+
+    var canvas = document.getElementsByTagName("canvas");
+    var W = 150;
+    var H = 150;
     var degrees = 0;
-    var new_degrees_1 = 0;
+    var new_degrees = 0;
     var difference = 9;
-    var color = "#ffe600"; //green looks better to me
+    var color = "#ffe600";
     var bgcolor = "#fff";
     var text;
-    var animation_loop;
-
-    function init(ctx)
-    {
-        //Clear the canvas everytime a chart is drawn
-        ctx.clearRect(0, 0, W, H);
-
-        //Background 360 degree arc
-        ctx.beginPath();
-        ctx.strokeStyle = bgcolor;
-        ctx.lineWidth = 5;
-        ctx.arc(W/2, H/2, 60, 0, Math.PI*2, false); //you can see the arc now
-        ctx.stroke();
-
-        //gauge will be a simple arc
-        //Angle in radians = angle in degrees * PI / 180
-        var radians = degrees * Math.PI / 180;
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = 5;
-        //The arc starts from the rightmost end. If we deduct 90 degrees from the angles
-        //the arc will start from the topmost end
-        ctx.arc(W/2, H/2, 60, 0 - 270*Math.PI/180, radians - 270*Math.PI/180, false);
-        //you can see the arc now
-        ctx.stroke();
-
-        //Lets add the text
-        ctx.fillStyle = color;
-        ctx.font = "35px Oswald_light";
-        text = Math.floor(degrees/360*100) + "%";
-        //Lets center the text
-        //deducting half of text width from position x
-        text_width = ctx.measureText(text).width;
-        //adding manual value to position y since the height of the text cannot
-        //be measured easily. There are hacks but we will keep it manual for now.
-        ctx.fillText(text, W/2 - text_width/2, H/2 + 15);
-    }
-
-    function draw(a)
-    {
+    function draw(new_degrees,ctx,animation_loop) {
         //Cancel any movement animation if a new chart is requested
         if(typeof animation_loop != undefined) clearInterval(animation_loop);
 
-        //random degree from 0 to 360
-        new_degrees_1 = a;
-        difference = new_degrees_1 - degrees;
-        //This will animate the gauge to new positions
-        //The animation will take 1 second
-        //time for each frame is 1sec / difference in degrees
-        animation_loop = setInterval(animate_to, 1000/difference);
+
+        difference = new_degrees - degrees;
+
+        animation_loop = setInterval(function () {
+            if(degrees > new_degrees)
+                clearInterval(animation_loop);
+
+            if(degrees <= new_degrees)
+                degrees++;
+            else
+                degrees--;
+
+            ctx.clearRect(0, 0, W, H);
+
+
+            ctx.beginPath();
+            ctx.strokeStyle = bgcolor;
+            ctx.lineWidth = 5;
+            ctx.arc(W/2, H/2, 60, 0, Math.PI*2, false); //you can see the arc now
+            ctx.stroke();
+
+
+            var radians = degrees * Math.PI / 180;
+            ctx.beginPath();
+            ctx.strokeStyle = color;
+            ctx.lineWidth = 5;
+
+            ctx.arc(W/2, H/2, 60, 0 - 270*Math.PI/180, radians - 270*Math.PI/180, false);
+            //you can see the arc now
+            ctx.stroke();
+
+            ctx.fillStyle = color;
+            ctx.font = "35px Oswald_light";
+            text = Math.floor(degrees/360*100) + "%";
+            text_width = ctx.measureText(text).width;
+            ctx.fillText(text, W/2 - text_width/2, H/2 + 15);
+
+        }, 1000/difference);
     }
 
-    //function to make the chart move to new degrees
-    function animate_to()
-    {
-        //clear animation loop if degrees reaches to new_degrees
-        if(degrees > new_degrees_1)
-            clearInterval(animation_loop);
-
-        if(degrees <= new_degrees_1)
-            degrees++;
-        else
-            degrees--;
-
-        init(canvas_context);
+    for(var i = 0; i < canvas.length; i++){
+        if(i == 0){
+            var canvas_context_0 = canvas[i].getContext("2d");
+            draw(324,canvas_context_0);
+        }
+        else if(i == 1){
+            var canvas_context_1 = canvas[i].getContext("2d");
+            draw(346,canvas_context_1);
+        }
+        else if(i == 2){
+            var canvas_context_2 = canvas[i].getContext("2d");
+            draw(306,canvas_context_2);
+        }
+        else if(i == 3){
+            var canvas_context_3 = canvas[i].getContext("2d");
+            draw(339,canvas_context_3);
+        }
     }
-
-    //Lets add some animation for fun
-    draw(324);
-
-
-
 
     //Открытие информации о члене команды
     var team_img = document.querySelectorAll(".team_item img");
