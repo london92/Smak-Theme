f77b4c1acedb4fca232880b3792e47371214971f
Nikolai Chmyr <nikolai.chmyr@gmail.com>
2016-08-14 19:54:04 +0300

    add some fixes

--- js/common.js
+++ js/common.js
@@ -1,5 +1,27 @@
 window.onload = function () {
-    
+    var linkNav = document.querySelectorAll("a"),
+        V = 0.5;  // скорость, может иметь дробное значение через точку
+    for (var i = 0; i < linkNav.length; i++) {
+        linkNav[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            var w = window.pageYOffset,  // прокрутка
+                hash = this.href.replace(/[^#]*(.*)/, '$1');  // id элемента, к которому нужно перейти
+            t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+                start = null;
+            requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+            function step(time) {
+                if (start === null) start = time;
+                var progress = time - start,
+                    r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+                window.scrollTo(0,r);
+                if (r != w + t) {
+                    requestAnimationFrame(step)
+                } else {
+                    location.hash = hash  // URL с хэшем
+                }
+            }
+        }, false);
+    }
     //для того, чтобы секция header автоматически вотображалась на всю высоту экрана 
     function  height_detect() {
         var header = document.getElementsByTagName("header")[0];
